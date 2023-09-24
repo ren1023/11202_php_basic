f73c7af88b9a669aa54f9ea707a320dfb8d6af83
ren1023 <renyou.1023@gmail.com>
2023-09-24 14:24:57 +0800

    calendar add

--- lesson5/date.php
+++ lesson5/date.php
@@ -23,26 +23,10 @@ echo $date1." 到 ".$date2 ." 有 ".$days." 天 ";
 $date="1974-10-7";
 $today= date("Y-m-d");
 
-$strdate=strtotime(date("n-j", strtotime($date)));//10-7
-$strtoday=strtotime(date("n-j", strtotime($today)));//10-24
-
-$diff=$strdate-$strtoday;//  "10-17"- "10-24"
+homework！
 
 
-echo $diff/(60*60*24);
-echo date("n-j", strtotime($date));
-echo "<br>";
-echo date("n-j", strtotime($today));
 
-if ( $diff < 0){
-    
-   $bir = (strtotime("+1 year",abs($diff)))/(60*60*24);
-    
-}else{
-    $bir=$bir/(60*60*24);
-} 
-
-echo $bir;
 ?>
 <hr>
 
@@ -70,7 +54,35 @@ if (date('l')=='Saturday'|| date('l')=='Sunday' ){
 }else{
     echo date('今天是西元 Y 年 m 月 d 日')."是上班日";
 }
+?>
+<hr>
+<h2>利用迴圈來計算連續五個周一的日期</h2>
+<ol>
+例:
+<li>2021-10-04 星期一</li>
+<li>2021-10-11 星期一</li>
+<li>2021-10-18 星期一</li>
+<li>2021-10-25 星期一</li>
+<li>2021-11-01 星期一</li>
+</ol>
 
+<?php
+$weekday=date('N');
+// echo $weekday;
+$diff=1-$weekday;//與星期二的差異
+$lastMonday=strtotime("$diff days");//計算上一個星期一的日期
+$nextMonday=date("Y-m-d",strtotime("+1 week",$lastMonday));//上一個星期一+1 週
+
+// echo $diff;
+
+for($i=0;$i<5;$i++){
+    
+    $nextMonday=date("Y-m-d",strtotime("+1 week",strtotime($nextMonday)));
+    echo "週一的日期：".$nextMonday;
+    echo "<br>";
+
+}
 
 
 ?>
+
