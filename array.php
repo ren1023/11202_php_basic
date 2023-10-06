2adeb6fabb6d7aeb5fc786ac0334b588a3293251
ren1023 <renyou.1023@gmail.com>
2023-10-06 14:26:19 +0800

    addarray

--- array.php
+++ array.php
@@ -13,7 +13,7 @@ $a[]='新莊';
 echo "<pre>";
 // 列印陣列
 print_r($a);
-echo "<pre>";
+echo "</pre>";
 
 // 陣列取值
 echo $a[0];
@@ -29,12 +29,73 @@ $b['最高學力']='生生幼稚園';
 $b['number']='0938123456';
 $b['血型']='O';
 
+echo '<pre>';
+print_r ($b);
+echo '</pre>';
+
 echo "<br>";
+echo '<pre>';
 echo $b['姓名'];
 echo $b['血型'];
+echo '</pre>';
+
+echo "<hr>";
+
+//多維陣列的呈現
+$c=[];
+$c['name']='小英';
+$c['興趣']=['看書','追劇','看yt'];
+
+echo "<pre>";
+print_r ($c);
+echo "</pre>";
+
+echo "<hr>";
+
+echo $c['name'];
+echo "<br>";
+// 取用二維陣列的值
+echo $c['興趣'][0];
+echo "<br>";
+echo $c['興趣'][2];
+
+
+
+
+
+
+
+
+
+echo "<pre>";
+echo "<hr>";
+echo "******程式的底端********";
+?>
+
+<h3>陣列</h3>
+
+<?php
+
+$a=['甲','乙','丙','丁'];
+
+
+for ($i=0; $i<count($a); $i++) {
+    echo $a[$i];
+}
 
 
 echo "<br>";
+
+foreach ($a as $b){
+    echo $b;
+}
+
 echo "<hr>";
-echo "******程式的底端********";
+
+
+foreach ($a as $idx => $b) {
+    echo "key:".$idx." value: ".$b;
+    echo "<br>";
+}
+
 ?>
